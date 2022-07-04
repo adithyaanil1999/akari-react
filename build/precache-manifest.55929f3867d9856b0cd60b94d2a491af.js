d76fa7662603ac6a290b64d4f21eb1322536b63b
adithya <adithyas.anil99@gmail.com>
2022-07-04 11:54:27 +0530

    v1 build

---
+++ build/precache-manifest.55929f3867d9856b0cd60b94d2a491af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d1ae4cb38d7c9904f95",
+    "url": "/static/css/main.69e19632.chunk.css"
+  },
+  {
+    "revision": "4d1ae4cb38d7c9904f95",
+    "url": "/static/js/main.4d1ae4cb.chunk.js"
+  },
+  {
+    "revision": "f1b498901fe9aa320db6",
+    "url": "/static/js/1.f1b49890.chunk.js"
+  },
+  {
+    "revision": "4243bb7f800f4c2fb0ee",
+    "url": "/static/js/2.4243bb7f.chunk.js"
+  },
+  {
+    "revision": "880d62563a23880b196b",
+    "url": "/static/js/runtime~main.880d6256.js"
+  },
+  {
+    "revision": "64353134d696fc28b4a2dd61fc4d8201",
+    "url": "/index.html"
+  }
+];
