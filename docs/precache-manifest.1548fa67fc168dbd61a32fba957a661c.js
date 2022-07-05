171d0c6f410b6acec518a92316778f7c20786671
adithya <adithyas.anil99@gmail.com>
2022-07-05 19:07:40 +0530

    build for pages v1

---
+++ docs/precache-manifest.1548fa67fc168dbd61a32fba957a661c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5f18e27546fa635b495",
+    "url": "/akari-react/static/css/main.69e19632.chunk.css"
+  },
+  {
+    "revision": "a5f18e27546fa635b495",
+    "url": "/akari-react/static/js/main.a5f18e27.chunk.js"
+  },
+  {
+    "revision": "f1b498901fe9aa320db6",
+    "url": "/akari-react/static/js/1.f1b49890.chunk.js"
+  },
+  {
+    "revision": "4243bb7f800f4c2fb0ee",
+    "url": "/akari-react/static/js/2.4243bb7f.chunk.js"
+  },
+  {
+    "revision": "b37a136dc81364aba84d",
+    "url": "/akari-react/static/js/runtime~main.b37a136d.js"
+  },
+  {
+    "revision": "8d576d9ac14fe77ea0e0309e7a16c32e",
+    "url": "/akari-react/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55929f3867d9856b0cd60b94d2a491af.js"
+  "/akari-react/precache-manifest.1548fa67fc168dbd61a32fba957a661c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/akari-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
